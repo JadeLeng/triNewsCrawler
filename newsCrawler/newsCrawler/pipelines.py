d1c23e795cfbe9a02a1eba2e00591908cfd42045
JadeLeng <showus2006@foxmail.com>
2017-06-18 02:40:45 +0800

    [Fixed] duplicate in mongodb

--- newsCrawler/newsCrawler/pipelines.py
+++ newsCrawler/newsCrawler/pipelines.py
@@ -16,14 +16,20 @@ class NewscrawlerPipeline(object):
         if item.get("news_thread",None) is None:
             print("item.get(thread)=none")
             return item
-        spec = {"news_thread":item["news_thread"],"isClassified":False}
+        spec = {"news_thread":item["news_thread"]}
         dictMerged = dict(item);
         dictMerged.update(dict2);
         if item['news_source'] == 'tencent':
+            if NewsDB.tencent.find(spec):
+                return
             NewsDB.tencent.update(spec,{"$set":dictMerged}, upsert = True)
         elif item['news_source'] == 'sina':
+            if NewsDB.sina.find(spec):
+                return
             NewsDB.sina.update(spec,{"$set":dictMerged}, upsert = True)
         else:
+            if NewsDB.netease.find(spec):
+                return
             NewsDB.netease.update(spec,{"$set":dictMerged}, upsert = True)
 	#print("update in mongodb")
         return None
