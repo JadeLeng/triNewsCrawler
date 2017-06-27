ec5d6391e57b14a2543dfeb44cc8b79648348857
JadeLeng <showus2006@foxmail.com>
2017-06-27 16:09:02 +0800

    [Modified]mysql

--- mysql.sql
+++ mysql.sql
@@ -9,3 +9,28 @@ CREATE TABLE `account` (
   UNIQUE KEY `email` (`email`)
   
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `cashacc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cashacc` (
+  `usrname` varchar(32) NOT NULL,
+  `car` float unsigned,
+  `domestic` float unsigned,
+  `education` float unsigned,
+  `entertainment` float unsigned,
+  `fin` float unsigned,
+  `game` float unsigned,
+  `international` float unsigned,
+  `internet` float unsigned,
+  `military` float unsigned,
+  `property` float unsigned,
+  `social` float unsigned,
+  `sports` float unsigned,
+  `tech` float unsigned,
+  `women` float unsigned,
+  KEY `usrname` (`usrname`),
+  CONSTRAINT `cashacc_ibfk_2` FOREIGN KEY (`usrname`) REFERENCES `account` (`usrname`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
